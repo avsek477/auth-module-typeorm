63704cf453a5eeb719967e1308b2462ceb6578bb
Abhishek <avsek.stha07@gmail.com>
2020-08-26 19:01:12 +0545

    cors config added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import { json } from 'body-parser';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
+import cors from 'cors';
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/sign-in';
@@ -10,6 +11,23 @@ import { signUpRouter } from './routes/sign-up';
 import { errorHandler, NotFoundError } from '@tmavisek/common';
 
 const app: Application = express ();
+
+const options: cors.CorsOptions = {
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'X-Access-Token',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: process.env.CLIENT_URL,
+    preflightContinue: false,
+};
+  
+//use cors middleware
+app.use(cors(options));
 
 app.set('trust proxy', true);
 app.use(json());
